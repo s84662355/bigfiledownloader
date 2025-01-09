77c00605ca65f1ae8a3d7db70262bc87ecc55a99
chenjiahao <271375247@qq.com>
2025-01-10 01:11:27 +0800

    9

--- example/main.go
+++ example/main.go
@@ -11,6 +11,6 @@ func main() {
 		log.Printf("下载进度: %.2f%%", 100*d)
 	})
 
-	err := ddd.Download(`https://sytg-browser.oss-ap-southeast-1.aliyuncs.com/CtrlFire-version/test/updateProgram6.66.zip`, "G:\\work\\bigfiledownloader\\example\\bigfiledownloader.zip")
+	err := ddd.Download(`https://sytxxxxxxxxxxeast-1.aliyuncs.com/Ctxion/test/updateProgram6.66.zip`, "G:\\work\\bigfiledownloader\\example\\bigfiledownloader.zip")
 	log.Println(err)
 }
