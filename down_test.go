e25964cc122edfc4564590a61963f9e508f375ab
s84662355 <271375247@qq.com>
2025-05-16 19:35:22 +0800

    Update down_test.go

--- down_test.go
+++ down_test.go
@@ -7,16 +7,16 @@ import (
 	"time"
 )
 
-// go test   -run TestDown -v -tags "dev"
+// go test   -run TestDown 
 func TestDown(t *testing.T) {
 	ctx, _ := context.WithTimeout(context.Background(), 5*60*time.Second)
 	err := NewBigDownloader(5, func(ddd float64) {
 		fmt.Println(ddd)
-	}).Download(ctx, `https://sytg-browser.oss-ap-southeast-1.aliyuncs.com/CtrlFire-version/test/updateProgram6.66.zip`, "1111sa.zip")
+	}).Download(ctx, `https://sytg-brow.aliyuncs.com/Ctrln/test/updateProgram6.66.zip`, "1111sa.zip")
 	fmt.Println(err)
 }
 
-// go test -v -run TestDownPng  -tags "dev"
+// go test -run TestDownPng 
 func TestDownPng(t *testing.T) {
 	err := NewBigDownloader(2, func(ddd float64) {
 		fmt.Println(ddd)
